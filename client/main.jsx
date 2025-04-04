53b65cd682044ca44af2263c19cb6070853feb57
Dennis1600 <dennis.neuman.7029@student.uu.se>
2025-04-04 15:16:44 +0200

    First commit on Frontend

---
+++ client/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // Byt tillbaka till ReactDOM om du vill
+import { Meteor } from 'meteor/meteor';
+import App from './ui/App';
+import './main.html';
+import { createRoot } from 'react-dom/client';
+
+
+
+Meteor.startup(() => {
+  console.log("Meteor startup running...");
+  
+  const container = document.getElementById('react-target');
+  console.log("Container:", container);
+
+  if (!container) {
+    console.error("❌ ERROR: 'react-target' not found!");
+    return;
+  }
+
+  const root = createRoot(container); 
+  root.render(<App />);
+});
