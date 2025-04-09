791d6f5a44a3ebfcad308d8edba5aaa052948f8c
Dennis1600 <dennis.neuman.7029@student.uu.se>
2025-04-09 16:50:07 +0200

    nu kan man gå från login till regg via logg-regg-knappen

--- client/ui/Webapp/imports/components/RegisterForm.jsx
+++ client/ui/Webapp/imports/components/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { LanguageSwitcher } from './langueSwitcher';
+import { LogRegSwitcher } from './LogRegSwitcher';
 
 export const RegisterForm = () => {
     const [username, setUsername] = useState('');
@@ -81,6 +82,7 @@ export const RegisterForm = () => {
             </div>
             <button type="submit" className="form-button">Register</button>
             <button onClick={() => toLogIn()}>Back to Login</button>
+            
         </form>
         </>
     );
