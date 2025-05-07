806f2320433c99ce28d0d4ffe2941cae24a5801a
Your Name <you@example.com>
2025-05-07 12:22:23 +0200

    Frontend and labels on reset-passwrod page

--- client/ui/Webapp/imports/pages/ResetPassword.jsx
+++ client/ui/Webapp/imports/pages/ResetPassword.jsx
@@ -1,45 +1,65 @@
 import { Accounts } from "meteor/accounts-base";
 import React, { useState } from "react";
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
+import { useTranslation } from "react-i18next";
+import { LanguageSwitcher } from "../components/LanguageSwitcher/langueSwitcher";
 
 export const ResetPassword = ({ token }) => {
   const [password, setPassword] = useState("");
   const [confirm, setConfirm] = useState("");
+  const [message, setMessage] = useState("");
+  const { t, i18n } = useTranslation();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (password !== confirm) {
-      alert("Lösenorden matchar inte");
+      setMessage(t("PasswordNoMatch"));
       return;
     }
 
     Accounts.resetPassword(token, password, (err) => {
       if (err) {
-        alert("Fel: " + err);
+        setMessage(t("SomethingWentWrong") + err);
       } else {
-        alert("Lösenordet är återställt!");
+        alert(t("PasswordReseted"));
         FlowRouter.go("/login");
       }
     });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Återställ lösenord</h2>
-      <input
-        type="password"
-        placeholder="Nytt lösenord"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Bekräfta lösenord"
-        value={confirm}
-        onChange={(e) => setConfirm(e.target.value)}
-      />
-      <button type="submit">Återställ</button>
-    </form>
+    <>
+      <LanguageSwitcher />
+      <div className="login-form">
+        <form onSubmit={handleSubmit}>
+          <h2>{t("ResetPassword")}</h2>
+          <input
+            type="password"
+            placeholder={t("newPassword")}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder={t("passwordConfirmNew")}
+            value={confirm}
+            onChange={(e) => setConfirm(e.target.value)}
+          />
+          <button className="form-button" type="submit">
+            {t("ResetPassword")}
+          </button>
+        </form>
+        {message && (
+          <p
+            style={{
+              color: message.includes("success") ? "green" : "red",
+            }}
+          >
+            {message}
+          </p>
+        )}
+      </div>
+    </>
   );
 };
