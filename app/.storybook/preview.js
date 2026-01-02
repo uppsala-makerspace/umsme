09303cc9a6b09849fbf3468cd7ba981e2980e12f
Matthias Palmer <mpalmer@gmail.com>
2026-01-02 11:13:07 +0100

    Add locale switcher toolbar to Storybook
    - Add globe icon toolbar to switch between Svenska and English
    - Use useEffect to update i18n language when locale changes
    - Set Swedish as default locale

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,11 +1,34 @@
+import { useEffect } from 'react';
 import '../client/main.css';
-import '../imports/i18n';
+import i18n from '../imports/i18n';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 /** @type { import('@storybook/react-webpack5').Preview } */
 const preview = {
-  decorators:
-    [(Story) => {
+  initialGlobals: {
+    locale: 'sv',
+  },
+  globalTypes: {
+    locale: {
+      name: 'Locale',
+      toolbar: {
+        icon: 'globe',
+        items: [
+          { value: 'sv', title: 'Svenska' },
+          { value: 'en', title: 'English' },
+        ],
+        showName: true,
+      },
+    },
+  },
+  decorators: [
+    (Story, context) => {
+      const { locale } = context.globals;
+
+      useEffect(() => {
+        i18n.changeLanguage(locale);
+      }, [locale]);
+
       return (
         <Router>
           <Routes>
@@ -17,7 +40,8 @@ const preview = {
           </Routes>
         </Router>
       );
-    }],
+    }
+  ],
   parameters: {
     controls: {
       matchers: {
