ff2a014e956b28ade52935ed778179f9a2b74456
Matthias Palmer <mpalmer@gmail.com>
2026-01-08 16:57:04 +0100

    Start script not needed, use npm run dev instead

--- app/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Also possible to provide settings via --settings settings.json
-export METEOR_SETTINGS=$(cat settings.json )
-export MAIL_URL=smtp://umsme@uppsalamakerspace.se:umsmerules4ever@mail.uppsalamakerspace.se:587?tls.rejectUnauthorized=false
-export MONGO_URL=mongodb://localhost:27017/umsme
-
-meteor --port 3001 --settings settings.json
-# NODE_DEBUG=net,stream
